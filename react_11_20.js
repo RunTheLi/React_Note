a009d55d5619eb7c7ffeaeeff92ca0a4665f4367
RunTheLi <lanmarock@gmail.com>
2025-01-30 12:31:14 +0700

    feat: add example code

---
+++ react_11_20.js
@@ -0,0 +1,214 @@
+// ## Compose React Components ##
+class Fruits extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h2>Fruits:</h2>
+        {/* Change code below this line */}
+        <NonCitrus />
+        <Citrus />
+        {/* Change code above this line */}
+      </div>
+    );
+  }
+}
+
+class TypesOfFood extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        {/* Change code below this line */}
+        <Fruits />
+        {/* Change code above this line */}
+        <Vegetables />
+      </div>
+    );
+  }
+}
+
+// ## Render a Class Component to the DOM ##
+class TypesOfFood extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div id="challenge-node">
+        <h1>Types of Food:</h1>
+        {/* Change code below this line */}
+        <Fruits />
+        <Vegetables />
+        {/* Change code above this line */}
+      </div>
+    );
+  }
+}
+
+// Change code below this line
+ReactDOM.render(<TypesOfFood />, document.getElementById("challenge-node"));
+
+// ## Write a React Component from Scratch ##
+// Change code below this line
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div id="challenge-node">
+        <h1>My First React Component!</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<MyComponent />, document.getElementById("challenge-node"));
+
+// ## Pass Props to a Stateless Functional Component ##
+const CurrentDate = (props) => {
+  return (
+    <div>
+      {/* Change code below this line */}
+      <p>The current date is: {props.date}</p>
+      {/* Change code above this line */}
+    </div>
+  );
+};
+
+class Calendar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>What date is it?</h3>
+        {/* Change code below this line */}
+        <CurrentDate date={Date()} />
+        {/* Change code above this line */}
+      </div>
+    );
+  }
+}
+
+
+// ## Pass an Array as Props ## 
+const List = (props) => {
+    { /* Change code below this line */ }
+    return <p>{props.tasks.join(", ")}</p>
+    { /* Change code above this line */ }
+  };
+  
+  class ToDo extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <h1>To Do Lists</h1>
+          <h2>Today</h2>
+          { /* Change code below this line */ }
+          <List tasks={["Buy groceries", "Do laundry"]} />
+          <h2>Tomorrow</h2>
+          <List tasks={["Workout", "Read a book", "Write code"]} />
+          { /* Change code above this line */ }
+        </div>
+      );
+    }
+  };
+
+// ## Use Default Props ## 
+const ShoppingCart = (props) => {
+    return (
+      <div>
+        <h1>Shopping Cart Component</h1>
+      </div>
+    )
+  };
+  // Change code below this line
+  ShoppingCart.defaultProps = { items: 0 }
+
+// ## Override Default Props ## 
+const Items = (props) => {
+    return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+  }
+  
+  Items.defaultProps = {
+    quantity: 0
+  }
+  
+  class ShoppingCart extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      { /* Change code below this line */ }
+      return <Items quantity={10}/>
+      { /* Change code above this line */ }
+    }
+  };
+
+// ## Use PropTypes to Define the Props You Expect ## 
+const Items = (props) => {
+    return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+  };
+  
+  // Change code below this line
+  Items.propTypes = {
+    quantity: PropTypes.number.isRequired
+  };
+  // Change code above this line
+  
+  Items.defaultProps = {
+    quantity: 0
+  };
+  
+  class ShoppingCart extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return <Items />
+    }
+  };
+
+// ## Access Props Using this.props ##
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+  
+    }
+    render() {
+      return (
+          <div>
+              { /* Change code below this line */ }
+              <Welcome name="Run"/>
+              { /* Change code above this line */ }
+          </div>
+      );
+    }
+  };
+  
+  class Welcome extends React.Component {
+    constructor(props) {
+      super(props);
+  
+    }
+    render() {
+      return (
+          <div>
+            { /* Change code below this line */ }
+            <p>Hello, <strong>{this.props.name}</strong>!</p>
+            { /* Change code above this line */ }
+          </div>
+      );
+    }
+  };
